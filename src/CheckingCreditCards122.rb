1aad882bc7f92741b39af208b0da1bac4cf0c0d3
kiwi <kiwi.swhite.coder@gmail.com>
2012-10-26 19:04:27 +0800

    repleac if else with proc #122

--- src/CheckingCreditCards122.rb
+++ src/CheckingCreditCards122.rb
@@ -1,6 +1,11 @@
 class CreditCard
+	
 	def initialize(num)
 		@num = num
+		begin_with(:Visa) { |num| num.length == 13 || num.length == 16 && num =~ /^4/ }
+		begin_with(:MasterCard) { |num| num.length == 16 && num =~ /^5[1-5]/ }
+		begin_with(:Discover) { |num| num.length == 16 && num =~ /^6011/ }
+		begin_with(:AMEX) { |num| num.length == 15 && num =~ /^(34|37)/ }
 	end
 
 	def validate?
@@ -8,20 +13,17 @@ class CreditCard
 		validate_divisble_by_10
 	end
 
+	def begin_with(card, &rule)
+		@cards ||= {}
+		@cards[card] = rule
+	end
+
 private
 	def validate_type
-		length = @num.size
-		if length == 15 && @num =~ /^(34|37)/
-			"AMEX"
-		elsif length == 16 && @num =~ /^6011/
-			"Discover"
-		elsif length == 16 && @num =~ /^5[1-5]/
-			"MasterCard"
-		elsif (length == 13 || length == 16) && @num =~ /^4/
-			"Visa"
-		else
-			"Unknown"
+		@cards.each_pair do |card, rule|
+			return true if rule.call(@num)
 		end
+		"Unknown"
 	end
 
 	def validate_divisble_by_10
@@ -32,7 +34,7 @@ private
 			else
 				sum += sum_digit((char.to_i).to_s)
 			end
-			multiply_two ^= true
+			multiply_two = !multiply_two
 			sum
 		end
 		sum % 10 == 0
