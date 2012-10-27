71d0f7c245a7e215e21c5cbc1a58940fb6001023
kiwi <kiwi.swhite.coder@gmail.com>
2012-10-27 08:40:27 +0800

    #122 insert rules use begin_with instead of insert rules at constructor

--- spec/CheckingCreditCards122_spec.rb
+++ spec/CheckingCreditCards122_spec.rb
@@ -7,15 +7,24 @@ class String
 end
 
 describe CreditCard do
+
+	before(:all) do
+		@card = CreditCard.new
+		@card.begin_with(:Visa) { |num| num.length == 13 || num.length == 16 && num =~ /^4/ }
+		@card.begin_with(:MasterCard) { |num| num.length == 16 && num =~ /^5[1-5]/ }
+		@card.begin_with(:Discover) { |num| num.length == 16 && num =~ /^6011/ }
+		@card.begin_with(:AMEX) { |num| num.length == 15 && num =~ /^(34|37)/ }
+	end
+
 	it "4408 0412 3456 7893 is validate" do
-		CreditCard.new("4408 0412 3456 7893".eliminate_space).validate?.should be_true
+		@card.validate_number?("4408 0412 3456 7893".eliminate_space).should be_true
 	end
 
 	it "1108 0412 3456 7893 is not validate because of the unknown credit card type" do
-		CreditCard.new("1108 0412 3456 7893".eliminate_space).validate?.should be_false
+		@card.validate_number?("1108 0412 3456 7893".eliminate_space).should be_false
 	end
 
 	it "4417 1234 5678 9112 is not validate because the sum is not divisble by 10" do
-		CreditCard.new("4417 1234 5678 9112".eliminate_space).validate?.should be_false
+		@card.validate_number?("4417 1234 5678 9112".eliminate_space).should be_false
 	end
 end
--- src/CheckingCreditCards122.rb
+++ src/CheckingCreditCards122.rb
@@ -1,39 +1,30 @@
 class CreditCard
 	
-	def initialize(num)
-		@num = num
-		begin_with(:Visa) { |num| num.length == 13 || num.length == 16 && num =~ /^4/ }
-		begin_with(:MasterCard) { |num| num.length == 16 && num =~ /^5[1-5]/ }
-		begin_with(:Discover) { |num| num.length == 16 && num =~ /^6011/ }
-		begin_with(:AMEX) { |num| num.length == 15 && num =~ /^(34|37)/ }
+	def initialize
+		@cards = {}
 	end
 
-	def validate?
-		return false if validate_type == "Unknown"
-		validate_divisble_by_10
+	def validate_number?(num)
+		return false if validate_type(num) == "Unknown"
+		validate_divisble_by_10(num)
 	end
 
 	def begin_with(card, &rule)
-		@cards ||= {}
 		@cards[card] = rule
 	end
 
 private
-	def validate_type
+	def validate_type(num)
 		@cards.each_pair do |card, rule|
-			return true if rule.call(@num)
+			return true if rule.call(num)
 		end
 		"Unknown"
 	end
 
-	def validate_divisble_by_10
+	def validate_divisble_by_10(num)
 		multiply_two = false
-		sum = @num.split(//).reverse.inject(0) do |sum, char|
-			if multiply_two
-				sum += sum_digit((char.to_i*2).to_s)
-			else
-				sum += sum_digit((char.to_i).to_s)
-			end
+		sum = num.split(//).reverse.inject(0) do |sum, char|
+			sum += sum_digit((char.to_i*(multiply_two ? 2:1)).to_s)
 			multiply_two = !multiply_two
 			sum
 		end
